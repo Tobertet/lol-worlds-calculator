cdffdc10e517300410c2f574079b0e01a4fe49af
Robert Mengual <robert.mengual@outlook.com>
2022-08-29 23:50:41 +0200

    Added tabs to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import { FC } from "react";
 import { LcsBracket } from "./components/LcsBracket";
+import { LecBracket } from "./components/LecBracket";
+import { Tabs } from "./components/Tabs";
 
-const App: FC = () => <LcsBracket />;
+const App: FC = () => (
+  <Tabs
+    tabs={[
+      {
+        id: "lec",
+        icon: "https://am-a.akamaihd.net/image?resize=60:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2F1592516184297_LEC-01-FullonDark.png",
+        content: <LecBracket />,
+      },
+      {
+        id: "lcs",
+        icon: "https://am-a.akamaihd.net/image?resize=60:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2FLCSNew-01-FullonDark.png",
+        content: <LcsBracket />,
+      },
+    ]}
+  />
+);
 
 export default App;
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,50 @@
+import { FC, ReactNode, useState } from "react";
+
+type Props = {
+  tabs: { id: "lec" | "lcs"; icon: string; content: ReactNode }[];
+};
+
+export const Tabs: FC<Props> = ({ tabs }) => {
+  const [selectedTab, setSelectedTab] = useState<"lec" | "lcs">("lec");
+  return (
+    <div style={{ marginTop: "104px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          background: "#131313",
+          position: "fixed",
+          top: 0,
+          width: "100%",
+          zIndex: 1,
+        }}
+      >
+        {tabs.map((tab) => (
+          <div
+            style={{
+              cursor: "pointer",
+              color: "white",
+              borderBottomWidth: 4,
+              borderBottomStyle: "solid",
+              borderBottomColor:
+                selectedTab === tab.id ? "#1A76E3" : "transparent",
+              display: "flex",
+              flexDirection: "column",
+              padding: "8px 16px",
+              alignItems: "center",
+              fontSize: "20px",
+            }}
+            key={tab.id}
+            onClick={() => {
+              setSelectedTab(tab.id);
+            }}
+          >
+            <img src={tab.icon} alt="asd" />
+            {tab.id.toUpperCase()}
+          </div>
+        ))}
+      </div>
+      {tabs.find((tab) => tab.id === selectedTab)?.content}
+    </div>
+  );
+};
