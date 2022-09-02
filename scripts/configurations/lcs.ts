b12b2af926d97ae2a424732c2c4ed9faf04841c7
Robert Mengual <robert.mengual@outlook.com>
2022-09-03 01:34:19 +0200

    Added scripts for generating lcs scenarios

---
+++ scripts/configurations/lcs.ts
@@ -0,0 +1,92 @@
+import {
+  ChampionshipConfiguration,
+  BracketSolver,
+  Match,
+  PointsSolver,
+  Standings,
+} from "../types";
+import { getLoser, getResult, getWinner } from "./utils";
+
+const lcsBracketSolver: BracketSolver = (scenario) => {
+  const match1: Match = { seed1: 5, seed2: 4, result: getResult(scenario, 1) };
+  const match2: Match = { seed1: 3, seed2: 6, result: getResult(scenario, 2) };
+  const match3: Match = {
+    seed1: 1,
+    seed2: getWinner(match1),
+    result: getResult(scenario, 3),
+  };
+  const match4: Match = {
+    seed1: 2,
+    seed2: getWinner(match2),
+    result: getResult(scenario, 4),
+  };
+  const match5: Match = {
+    seed1: getLoser(match2),
+    seed2: 7,
+    result: getResult(scenario, 5),
+  };
+  const match6: Match = {
+    seed1: getLoser(match1),
+    seed2: 8,
+    result: getResult(scenario, 6),
+  };
+  const match7: Match = {
+    seed1: getLoser(match3),
+    seed2: getWinner(match5),
+    result: getResult(scenario, 7),
+  };
+  const match8: Match = {
+    seed1: getLoser(match4),
+    seed2: getWinner(match6),
+    result: getResult(scenario, 8),
+  };
+  const match9: Match = {
+    seed1: getWinner(match3),
+    seed2: getWinner(match4),
+    result: getResult(scenario, 9),
+  };
+  const match10: Match = {
+    seed1: getWinner(match7),
+    seed2: getWinner(match8),
+    result: getResult(scenario, 10),
+  };
+  const match11: Match = {
+    seed1: getLoser(match9),
+    seed2: getWinner(match10),
+    result: getResult(scenario, 11),
+  };
+  const match12: Match = {
+    seed1: getWinner(match9),
+    seed2: getWinner(match11),
+    result: getResult(scenario, 12),
+  };
+  return {
+    scenario,
+    standings: {
+      1: getWinner(match12),
+      2: getLoser(match12),
+      3: getLoser(match11),
+      4: getLoser(match10),
+    },
+  };
+};
+
+const convertStandings = (standings: Standings): Standings => {
+  let newStandings: Standings = [];
+  for (const key of Object.keys(standings)) {
+    newStandings[parseInt(key) - 1] = standings[key];
+  }
+  return newStandings;
+};
+
+const lcsPointsSolver: PointsSolver = (playoffsResult) => ({
+  ...playoffsResult,
+  standings: convertStandings(playoffsResult.standings),
+});
+
+export const lcsConfiguration: ChampionshipConfiguration = {
+  totalMatches: 12,
+  totalTeams: 8,
+  bracketSolver: lcsBracketSolver,
+  pointsSolver: lcsPointsSolver,
+};
--- scripts/generate.ts
+++ scripts/generate.ts
@@ -3,15 +3,18 @@ import { ChampionshipConfiguration } from "./types";
 import { lplConfiguration } from "./configurations/lpl";
 import { argv } from "process";
 import { lecConfiguration } from "./configurations/lec";
+import { lcsConfiguration } from "./configurations/lcs";
 
 type AvailableConfigurations = {
   lpl: ChampionshipConfiguration;
   lec: ChampionshipConfiguration;
+  lcs: ChampionshipConfiguration;
 };
 
 const availableConfigurations: AvailableConfigurations = {
   lpl: lplConfiguration,
   lec: lecConfiguration,
+  lcs: lcsConfiguration,
 };
 
 if (!argv[2]) {
